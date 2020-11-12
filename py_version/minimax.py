688d56ab1f3113c36e2993fc4a767e5c1bce3920
Alicia Cheng <qinglin@ualberta.ca>
2020-11-12 04:33:07 -0700

    Refactor classes and methods; Test functionality
    Change methods self.wins() and self.empty_cells() back to functions
    Modify small changes in main() so it adopts the new classes
    minimax.py adopts all changes and passes all test cases

--- py_version/minimax.py
+++ py_version/minimax.py
@@ -31,56 +31,37 @@ class board():
             ]
 
 
-    def wins(self, player):
-        """
-        This function tests if a specific player wins. Possibilities:
-        * Three rows    [X X X] or [O O O]
-        * Three cols    [X X X] or [O O O]
-        * Two diagonals [X X X] or [O O O]
-        :param state: the state of the current board
-        :param player: a human or a computer
-        :return: True if the player wins
-        """
-        state = self.get_board()
-        win_state = [
-            [state[0][0], state[0][1], state[0][2]],
-            [state[1][0], state[1][1], state[1][2]],
-            [state[2][0], state[2][1], state[2][2]],
-            [state[0][0], state[1][0], state[2][0]],
-            [state[0][1], state[1][1], state[2][1]],
-            [state[0][2], state[1][2], state[2][2]],
-            [state[0][0], state[1][1], state[2][2]],
-            [state[2][0], state[1][1], state[0][2]],
-        ]
-        if [player, player, player] in win_state:
-            return True
-        else:
-            return False
+    def get_board(self):
+        return(self.board)
 
-    def game_over(self):
+    def get_HUMAN(self):
+        return(self.HUMAN)
+
+    def get_COMP(self):
+        return(self.COMP)
+
+    def evaluate(self, state):
+        """
+        Function to heuristic evaluation of state.
+        :param state: the state of the current board
+        :return: +1 if the computer wins; -1 if the human wins; 0 draw
+        """
+        if wins(state, self.get_COMP()):
+            score = +1
+        elif wins(state, self.get_HUMAN()):
+            score = -1
+        else:
+            score = 0
+    
+        return score
+
+    def game_over(self, state):
         """
         This function test if the human or computer wins
         :param state: the state of the current board
         :return: True if the human or computer wins
         """
-        return self.wins(self.get_HUMAN()) or self.wins(self.get_COMP())
-
-
-    def empty_cells(self):
-        """
-        Each empty cell will be added into cells' list
-        :param state: the state of the current board
-        :return: a list of empty cells
-        """
-        cells = []
-    
-        for x, row in enumerate(self.get_board()):
-            for y, cell in enumerate(row):
-                if cell == 0:
-                    cells.append([x, y])
-    
-        return cells
-
+        return wins(state, self.get_HUMAN()) or wins(state, self.get_COMP())
 
     def valid_move(self, x, y):
         """
@@ -130,6 +111,48 @@ class board():
             print('\n' + str_line)
 
 
+    def __str__(self):
+            return 'board = ' + str(self.get_board())
+    
+    
+    def __repr__(self):
+            identity = 'the id is {}'.format(id(self))
+            return identity
+
+    def minimax(self, state, depth, player):
+        """
+        AI function that choice the best move
+        :param state: current state of the board
+        :param depth: node index in the tree (0 <= depth <= 9),
+        but never nine in this case (see iaturn() function)
+        :param player: an human or a computer
+        :return: a list with [the best row, best col, best score]
+        """
+        if player == self.get_COMP():
+            best = [-1, -1, -infinity]
+        else:
+            best = [-1, -1, +infinity]
+    
+        if depth == 0 or self.game_over(state):
+            score = self.evaluate(state)
+            return [-1, -1, score]
+    
+        for cell in empty_cells(state):
+            x, y = cell[0], cell[1]
+            state[x][y] = player
+            score = self.minimax(state, depth - 1, -player)
+            state[x][y] = 0
+            score[0], score[1] = x, y
+    
+            if player == self.get_COMP():
+                if score[2] > best[2]:
+                    best = score  # max value
+            else:
+                if score[2] < best[2]:
+                    best = score  # min value
+
+        return best
+
     def ai_turn(self, c_choice, h_choice):
         """
         It calls the minimax function if the depth < 9,
@@ -138,10 +161,10 @@ class board():
         :param h_choice: human's choice X or O
         :return:
         """
-        depth = len(self.empty_cells())
-        if depth == 0 or self.game_over():
+        depth = len(empty_cells(self.get_board()))
+        if depth == 0 or self.game_over(self.get_board()):
             return
-
+    
         clean()
         print(f'Computer turn [{c_choice}]')
         self.render(c_choice, h_choice)
@@ -150,26 +173,23 @@ class board():
             x = choice([0, 1, 2])
             y = choice([0, 1, 2])
         else:
-            COMP = self.get_COMP()
-
-            state1 = state(self.get_board())
-            move = state1.minimax(state1.get_board(), depth, COMP)
+            move = self.minimax(self.get_board(), depth, self.get_COMP())
             x, y = move[0], move[1]
     
-        self.set_move(x, y, COMP)
+        self.set_move(x, y, self.get_COMP())
         # Paul Lu.  Go full speed.
         # time.sleep(1)
 
 
-    def human_turn(self.c_choice, h_choice):
+    def human_turn(self, c_choice, h_choice):
         """
         The Human plays choosing a valid move.
         :param c_choice: computer's choice X or O
         :param h_choice: human's choice X or O
         :return:
         """
-        depth = len(self.empty_cells())
-        if depth == 0 or self.game_over():
+        depth = len(empty_cells(self.get_board()))
+        if depth == 0 or self.game_over(self.get_board()):
             return
     
         # Dictionary of valid moves
@@ -200,119 +220,47 @@ class board():
                 print('Bad choice')
 
 
-    def __str__(self):
-        return 'board = ' + str(self.get_board())
-    
-    
-    def __repr__(self):
-        identity = 'the id is {}'.format(id(self))
-        return identity
 
-
-class state():
-    def __init__(self, state):
-        self.board = state
-        self.HUMAN = -1
-        self.COMP = +1
-
-
-    def evaluate(self, state):
-        """
-        Function to heuristic evaluation of state.
-        :param state: the state of the current board
-        :return: +1 if the computer wins; -1 if the human wins; 0 draw
-        """
-
-        if wins(self.get_COMP):
-            score = +1
-        elif wins(self.get_HUMAN):
-            score = -1
-        else:
-            score = 0
-    
-        return score
-
-
-    def minimax(self, state, depth, player):
-        """
-        AI function that choice the best move
-        :param state: current state of the board
-        :param depth: node index in the tree (0 <= depth <= 9),
-        but never nine in this case (see iaturn() function)
-        :param player: an human or a computer
-        :return: a list with [the best row, best col, best score]
-        """
-        if player == self.get_COMP():
-            best = [-1, -1, -infinity]
-        else:
-            best = [-1, -1, +infinity]
-    
-        if depth == 0 or self.game_over():
-            self.board = state
-            score = self.evaluate()
-            return [-1, -1, score]
-    
-        for cell in self.empty_cells():
-            x, y = cell[0], cell[1]
-            state[x][y] = player
-
-            score = self.minimax(state, depth - 1, -player)
-            state[x][y] = 0
-            score[0], score[1] = x, y
-    
-            if player == self.get_COMP():
-                if score[2] > best[2]:
-                    best = score  # max value
-            else:
-                if score[2] < best[2]:
-                    best = score  # min value
-    
-        return best
-
-    
-    def __str__(self):
-        return 'current state = ' + str(self.get_state())
-
-
-    def __repr__(self):
-        identity = 'the id is {}'.format(id(self))
-        return identity
-
-
-
-def minimax(state, depth, player):
+def wins(state, player):
     """
-    AI function that choice the best move
-    :param state: current state of the board
-    :param depth: node index in the tree (0 <= depth <= 9),
-    but never nine in this case (see iaturn() function)
-    :param player: an human or a computer
-    :return: a list with [the best row, best col, best score]
+    This function tests if a specific player wins. Possibilities:
+    * Three rows    [X X X] or [O O O]
+    * Three cols    [X X X] or [O O O]
+    * Two diagonals [X X X] or [O O O]
+    :param state: the state of the current board
+    :param player: a human or a computer
+    :return: True if the player wins
     """
-    if player == COMP:
-        best = [-1, -1, -infinity]
+    win_state = [
+        [state[0][0], state[0][1], state[0][2]],
+        [state[1][0], state[1][1], state[1][2]],
+        [state[2][0], state[2][1], state[2][2]],
+        [state[0][0], state[1][0], state[2][0]],
+        [state[0][1], state[1][1], state[2][1]],
+        [state[0][2], state[1][2], state[2][2]],
+        [state[0][0], state[1][1], state[2][2]],
+        [state[2][0], state[1][1], state[0][2]],
+    ]
+    if [player, player, player] in win_state:
+        return True
     else:
-        best = [-1, -1, +infinity]
+        return False
 
-    if depth == 0 or game_over(state):
-        score = evaluate(state)
-        return [-1, -1, score]
 
-    for cell in empty_cells(state):
-        x, y = cell[0], cell[1]
-        state[x][y] = player
-        score = minimax(state, depth - 1, -player)
-        state[x][y] = 0
-        score[0], score[1] = x, y
+def empty_cells(state):
+    """
+    Each empty cell will be added into cells' list
+    :param state: the state of the current board
+    :return: a list of empty cells
+    """
+    cells = []
 
-        if player == COMP:
-            if score[2] > best[2]:
-                best = score  # max value
-        else:
-            if score[2] < best[2]:
-                best = score  # min value
+    for x, row in enumerate(state):
+        for y, cell in enumerate(row):
+            if cell == 0:
+                cells.append([x, y])
 
-    return best
+    return cells
 
 
 def clean():
@@ -328,7 +276,6 @@ def clean():
         system('cls')
     else:
         system('clear')
-
 
 
 def main():
@@ -374,10 +321,7 @@ def main():
 
     # Main loop of this game
     board1 = board()
-    HUMAN = board1.get_HUMAN()
-    COMP = board1.get_COMP()
-
-    while len(board1.empty_cells()) > 0 and not board1.game_over():
+    while len(empty_cells(board1.get_board())) > 0 and not board1.game_over(board1.get_board()):
         if first == 'N':
             board1.ai_turn(c_choice, h_choice)
             first = ''
@@ -386,12 +330,15 @@ def main():
         board1.ai_turn(c_choice, h_choice)
 
     # Game over message
-    if board1.wins(HUMAN):
+    boardd = board1.get_board()
+    HUMAN = board1.get_HUMAN()
+    COMP = board1.get_COMP()
+    if wins(boardd, HUMAN):
         clean()
         print(f'Human turn [{h_choice}]')
         board1.render(c_choice, h_choice)
         print('YOU WIN!')
-    elif board1.wins(COMP):
+    elif wins(boardd, COMP):
         clean()
         print(f'Computer turn [{c_choice}]')
         board1.render(c_choice, h_choice)
@@ -402,7 +349,6 @@ def main():
         print('DRAW!')
 
     exit()
-
 
 if __name__ == '__main__':
     main()
